413061050d691659adae6e012d5265a07247eaf7
Mpho <mabidikamampho1@gmail.com>
2023-07-18 22:20:34 +0200

    First commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,4 +1,18 @@
+import chalk from 'chalk';
+//import the greet module that is in the current folder
 import greet from './greet.js'
 
-// call the function & print the result on the screen
-console.log(greet('Xola'))
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+
+import figlet from 'figlet';
+
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
